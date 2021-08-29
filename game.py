b55b6c9edee34f9a5fc6c1a7eb8848deb8d8837e
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2021-08-29 01:18:17 -0400

    Initial commit

---
+++ game.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+def main():
+    pass
+
+# Guard to prevent this script from being executed when imported. "__name__" won't equal
+# "__main__" unless this script is run from the command line to protect it from running.
+if __name__ == "__main__":
+    main()
